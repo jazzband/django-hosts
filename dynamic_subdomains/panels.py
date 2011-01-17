439b52c2f630340ac9a4d684ee914c38124b2515
Chris Lamb <chris@playfire.com>
2011-01-17 20:26:33 +0000

    Disable fix_location_header locally.
    Signed-off-by: Chris Lamb <chris@playfire.com>

--- dynamic_subdomains/panels.py
+++ dynamic_subdomains/panels.py
@@ -1,7 +1,9 @@
 from debug_toolbar.panels import DebugPanel
 
+from django.http.utils import fix_location_header
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
+from django.core.handlers.base import BaseHandler
 
 class SubdomainPanel(DebugPanel):
     """
@@ -33,3 +35,16 @@ class SubdomainPanel(DebugPanel):
         request.META.pop('HTTP_HOST', '')
         if self.domain:
             request.META['HTTP_HOST'] = self.domain
+
+            # django.http.utils.fix_location_header rewrites any relative
+            # Location header to an absolute one. For example:
+            #
+            #    Location: /foo   ==>   Location: http://sub.example.com/foo
+            #
+            # This causes problems when testing subdomains locally so we remove
+            # it here.
+
+            try:
+                BaseHandler.response_fixes.remove(fix_location_header)
+            except ValueError:
+                pass
