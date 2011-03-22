de22119df5131d8f0546eef577d6daf6f6a7b219
Chris Lamb <chris@playfire.com>
2011-03-22 12:00:13 +0000

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-dynamic-subdomains',
+    description="Dynamic and static subdomain support for Django.",
+    version='0.1',
+    url='http://code.playfire.com/',
+
+    author='Playfire.com',
+    author_email='tech@playfire.com',
+    license='BSD',
+
+    packages=find_packages(),
+)
