70d7166a7daf378128ef1d1195c7ffaf2857de75
Jannis Leidel <jannis@leidel.info>
2011-05-29 17:42:41 +0200

    Added install_requires option and added classifiers.

--- setup.py
+++ setup.py
@@ -5,15 +5,26 @@ setup(
     description="Dynamic and static hosts support for Django.",
     version='0.1',
     url='https://github.com/jezdez/django-hosts',
-
     author='Jannis Leidel',
     author_email='jannis@leidel.info',
     license='BSD',
-
     packages=find_packages(),
     package_data={
         'hosts': [
             'contrib/toolbar/templates/hosts/*.html',
         ],
     },
+    install_requires = ['django >= 1.1, <1.4'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
