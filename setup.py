699602fbb162f286f4a02fecbbdf748a4f747850
Jannis Leidel <jannis@leidel.info>
2011-05-29 18:15:05 +0200

    Added long description to setup.py.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='django-hosts',
     description="Dynamic and static hosts support for Django.",
+    long_description=open('README.rst').read(),
     version='0.1',
     url='https://github.com/jezdez/django-hosts',
     author='Jannis Leidel',
