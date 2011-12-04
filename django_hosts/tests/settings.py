b5bb72e749f48e7729d61d6bd7398d5636cf2feb
Jannis Leidel <jannis@leidel.info>
2011-12-04 13:43:09 +0100

    Use django-jenkins instead.

--- django_hosts/tests/settings.py
+++ django_hosts/tests/settings.py
@@ -5,8 +5,16 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django_hosts',
     'django_hosts.tests',
+    'django_jenkins',
 ]
 
 ROOT_URLCONF = 'django_hosts.tests.urls.root'
 
 SITE_ID = 1
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',
+)
