9966bce435a4d8fe5c46631663a975bfb0b3ee78
Jannis Leidel <jannis@leidel.info>
2012-02-14 12:49:48 +0100

    Actually test the tests app only again since we have some requirements that can't be met by pristine Django projects.

---
+++ django_hosts/tests/tests.py
@@ -0,0 +1,5 @@
+from .middleware import MiddlewareTests
+from .reverse import UtilityTests, ReverseTest
+from .templatetags import TemplateTagsTest
+from .defaults import PatternsTests, HostTests
+from .sites import SitesTests
