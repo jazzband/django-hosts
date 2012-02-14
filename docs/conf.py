d0a95d4f39d6d0b5bfe08c7b14ec69177ba32b38
Jannis Leidel <jannis@leidel.info>
2012-02-14 12:57:45 +0100

    Bumped year.

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'django-hosts'
-copyright = u'2011, Jannis Leidel and contributors'
+copyright = u'2011-2012, Jannis Leidel and contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
