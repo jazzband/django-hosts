2735d4a7b1ae0af0d58ea0accb973ab11477783e
mvantellingen <michaelvantellingen@gmail.com>
2013-05-14 17:08:04 +0200

    Use django.shortcuts.render instead of TemplateView

--- django_hosts/tests/urls/simple.py
+++ django_hosts/tests/urls/simple.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import TemplateView
 
 urlpatterns = patterns('django.views.generic.simple',
-    url(r'^simple/$', TemplateView.as_view(), name='simple-direct'),
+    url(r'^simple/$', 'django.shortcuts.render', name='simple-direct'),
 )
