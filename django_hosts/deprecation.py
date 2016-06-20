ee137e396d7814cb8b10ed9cf9343c591fda2f65
Brett Haydon <brett@haydon.id.au>
2016-06-20 20:52:50 +1000

    Refs #57. Private model._meta method get_all_field_names removed in django 1.10

--- django_hosts/deprecation.py
+++ django_hosts/deprecation.py
@@ -30,3 +30,26 @@ else:
             return actual_get_callable(lookup_view, can_fail)
         except ViewDoesNotExist as exc:
             raise ImproperlyConfigured(exc.args[0].replace('View', 'Callable'))
+
+if VERSION[0:2] >= (1, 10):
+    def get_all_field_names(meta):
+        names = set()
+        fields = meta.get_fields()
+        for field in fields:
+            # For backwards compatibility GenericForeignKey should not be
+            # included in the results.
+            if field.is_relation and field.many_to_one and field.related_model is None:
+                continue
+            # Relations to child proxy models should not be included.
+            if (field.model != meta.model and
+                    field.model._meta.concrete_model == meta.concrete_model):
+                continue
+
+            names.add(field.name)
+            if hasattr(field, 'attname'):
+                names.add(field.attname)
+        return list(names)
+
+else:
+    def get_all_field_names(meta):
+        return meta.get_all_field_names()
