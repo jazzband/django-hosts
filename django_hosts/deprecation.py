b269bcda456f356f401876c4c6c11ec9215790af
Brett Haydon <brett@haydon.id.au>
2016-06-20 20:51:26 +1000

    Refs #57. get_callable can_fail arg removed in django 1.10

--- django_hosts/deprecation.py
+++ django_hosts/deprecation.py
@@ -1,4 +1,5 @@
 from django import VERSION
+from django.core.exceptions import ImproperlyConfigured, ViewDoesNotExist
 
 
 if VERSION[0:2] >= (1, 10):
@@ -7,3 +8,25 @@ if VERSION[0:2] >= (1, 10):
 else:
     class MiddlewareMixin(object):
         pass
+
+
+if VERSION[0:2] >= (1, 10):
+    from django.urls.utils import get_callable as actual_get_callable
+    
+    def get_callable(lookup_view):
+        try:
+            return actual_get_callable(lookup_view)
+        except ViewDoesNotExist as exc:
+            raise ImproperlyConfigured(exc.args[0].replace('View', 'Callable'))
+
+else:
+    from django.core.urlresolvers import get_callable as actual_get_callable
+
+    def get_callable(lookup_view, can_fail=False):
+        """
+        can_fail is DEPRECATED
+        """
+        try:
+            return actual_get_callable(lookup_view, can_fail)
+        except ViewDoesNotExist as exc:
+            raise ImproperlyConfigured(exc.args[0].replace('View', 'Callable'))
