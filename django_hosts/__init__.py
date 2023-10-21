15d25806c80905a64992aef8681b67d6dc0cc3ba
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2023-10-21 10:49:50 -0400

    Dropped Python 3.7 (#164)
    Also removed usage of `pkg_resources` in `django_hosts/__init__.py`, as
    it's been deprecated (see the "Attention" notice at the top of
    https://setuptools.pypa.io/en/latest/pkg_resources.html).
    `importlib.metadata.version()` was added in Python 3.8, which is the
    new, recommended way to get the version of an installed package.
    
    Co-authored-by: Anders <6058745+ddabble@users.noreply.github.com>

--- django_hosts/__init__.py
+++ django_hosts/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-import pkg_resources
+import importlib.metadata
 
 try:  # pragma: no cover
     from django_hosts.defaults import patterns, host
@@ -8,5 +8,5 @@ try:  # pragma: no cover
 except ImportError:  # pragma: no cover
     pass
 
-__version__ = pkg_resources.get_distribution('django-hosts').version
+__version__ = importlib.metadata.version('django-hosts')
 __author__ = 'Jazzband members (https://jazzband.co/)'
