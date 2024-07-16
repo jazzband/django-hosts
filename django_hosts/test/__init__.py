e247a6dddec171290c572a2492c4611610208600
Bruno Alla <alla.brunoo@gmail.com>
2024-07-16 18:27:17 +0100

    Make test classes easier to import

---
+++ django_hosts/test/__init__.py
@@ -0,0 +1,10 @@
+from .client import AsyncHostsClient, HostsClient
+from .testcases import HostsTestCase, SimpleHostsTestCase, TransactionHostsTestCase
+
+__all__ = [
+    'AsyncHostsClient',
+    'HostsClient',
+    'HostsTestCase',
+    'SimpleHostsTestCase',
+    'TransactionHostsTestCase',
+]
