957f8a97bdcfddebdef6f8734894eff9169ae315
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2025-12-10 15:49:10 +0300

    Apply black fixes

--- tests/hosts/blank.py
+++ tests/hosts/blank.py
@@ -1,7 +1,7 @@
 from django_hosts import patterns, host
 
 host_patterns = patterns(
-    '',
-    host(r'', 'tests.urls.simple', name='blank'),
-    host(r'|www', 'tests.urls.simple', name='blank_or_www'),
+    "",
+    host(r"", "tests.urls.simple", name="blank"),
+    host(r"|www", "tests.urls.simple", name="blank_or_www"),
 )
